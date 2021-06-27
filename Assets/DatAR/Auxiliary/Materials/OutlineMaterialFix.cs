fc7db0749853ad04f5f4ed76a7a36e0261101f0c
Norazan <thendjes17@gmail.com>
2021-06-28 01:54:04 +0200

    Add hover outline to resourcespheres

---
+++ Assets/DatAR/Auxiliary/Materials/OutlineMaterialFix.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// This fix is necessary due since <see cref="Microsoft.MixedReality.Toolkit.Utilities.MeshOutline" /> does not respect dynamically assigned materials. 
+/// The materials in the topic model spheres are dynamically assigned.
+public class OutlineMaterialFix : MonoBehaviour
+{
+    private Material[] oldMaterials;
+
+    public void StorePreviousMaterials()
+    {
+        oldMaterials = GetComponent<MeshRenderer>().materials;
+    }
+
+    public void RestorePreviousMaterials()
+    {
+        GetComponent<MeshRenderer>().materials = oldMaterials;
+    }
+}
