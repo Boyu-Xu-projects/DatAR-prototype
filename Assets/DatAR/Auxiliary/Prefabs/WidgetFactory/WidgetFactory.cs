757d5286a2b562571ea5b41f057ce308df1e4fed
tanhaee <tanhaee_ghazal@yahoo.com>
2021-08-24 14:59:15 +0200

    Add duplication feature and arrange the widgets positions

--- Assets/DatAR/Auxiliary/Prefabs/WidgetFactory/WidgetFactory.cs
+++ Assets/DatAR/Auxiliary/Prefabs/WidgetFactory/WidgetFactory.cs
@@ -8,49 +8,38 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+
 public class WidgetFactory : MonoBehaviour
 {
-    [FormerlySerializedAs("isFixed")] public bool shouldDuplicateOnGrab = true;
-    [SerializeField] private GameObject prefabToGenerate;
 
-    private ResourceSphereManufacturer _manufacturer;
+    [SerializeField] private GameObject prefabToGenerate;
+    public bool shouldDuplicateOnGrab = true;
 
     private void Awake()
     {
-        var resourceSpherePool = GameObject.Find("StandaloneWidgetPool");
+        var widgetPool = GameObject.Find("StandaloneWidgetPool");
 
-        if (!resourceSpherePool)
+        if (!widgetPool)
         {
             Debug.LogError("No `StandaloneWidgetPool` Prefab was not found in the scene. "
-                           + "This GameObject is required to allow free-floating resource spheres.");
+                           + "This GameObject is required to allow widget generation");
             gameObject.SetActive(false);
-            return;
         }
-
-        _manufacturer = resourceSpherePool.GetComponent<ResourceSphereManufacturer>();
     }
-    public void DuplicateIfFixed()
+    public void GenerateWidget()
     {
         if (shouldDuplicateOnGrab)
         {
-            var duplicate = Instantiate(prefabToGenerate, GameObject.Find("StandaloneWidgetPool").transform);
+            var duplicate = Instantiate(prefabToGenerate, transform.parent);
             duplicate.transform.position = transform.position;
             duplicate.transform.localScale = transform.localScale;
             duplicate.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
 
             transform.SetParent(GameObject.Find("StandaloneWidgetPool").transform, true);
-            transform.localScale = new Vector3(_manufacturer.pointScale, _manufacturer.pointScale, _manufacturer.pointScale);
-            GetComponent<WidgetFactory>().shouldDuplicateOnGrab = false;
-
-
-            // If not fixed, simply move when picked up.
-            //public void GenerateWidget()
-            //{
-            //    var duplicate = Instantiate(prefabToGenerate, GameObject.Find("StandaloneWidgetPool").transform);
-            //    var hand = transform.parent.GetComponent<Hand>();
-            //    hand.DetachObject(gameObject);
-            //    hand.AttachObject(duplicate, hand.GetGrabStarting());
-            //}
+            shouldDuplicateOnGrab = false;
         }
+        //transform.localScale = new Vector3(_manufacturer.pointScale, _manufacturer.pointScale, _manufacturer.pointScale);
+        //GameObject.Find("StandaloneWidgetPool").transform
+
     }
 }
