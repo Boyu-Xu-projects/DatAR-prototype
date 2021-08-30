9c96fa6862f5217e2a0d00d2384ac216d6e55fa9
Thijs Hendrickx <thendjes17@gmail.com>
2021-08-30 15:21:05 +0200

    Fix pointer draw queue warning, disable gaze pointer on startup.

--- Assets/DatAR/Scripts/DisableDevelopmentConsole.cs
+++ Assets/DatAR/Scripts/DisableDevelopmentConsole.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DisableDevelopmentConsole : MonoBehaviour
 {
+    private void Start()
+    {
+        // Disable to GazeProvider as it isn't relevant to DatAR and gets in the way of the mesh outline generation.
+        var gazeProvider = Camera.main.GetComponent<GazeProvider>();
+        if(gazeProvider != null)
+        {
+            gazeProvider.enabled = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
-    {
+    {   // Only necessary if built using the Debug configuration, as Unity inserts a weird
+        // developer console every update in this config. This is the only way to turn it off.
         Debug.developerConsoleVisible = false;
     }
 }
