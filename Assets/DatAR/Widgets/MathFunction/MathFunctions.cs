0beb86b2eac4001475642bc86a88ed8bb1b5e8ec
tanhaee <tanhaee_ghazal@yahoo.com>
2022-08-24 17:07:28 +0200

    Writing Math commands

--- Assets/DatAR/Widgets/MathFunction/MathFunctions.cs
+++ Assets/DatAR/Widgets/MathFunction/MathFunctions.cs
@@ -15,10 +15,10 @@ using Newtonsoft.Json;
 
 namespace DatAR.Widgets.MathFunctions
 { 
-    public class MathFunctions : MonoBehaviour//, //IQueryState
+    public class MathFunctions : MonoBehaviour, IQueryState
     {
-        //public BehaviorSubject<QueryState> IsLoading { get; }
-        //public string ErrorMessage { get; private set; }
+        public BehaviorSubject<QueryState> IsLoading { get; }
+        public string ErrorMessage { get; private set; }
 
         public DataflowInletL dataReceiver;
         public DataflowOutletL dataSender;
@@ -31,14 +31,17 @@ namespace DatAR.Widgets.MathFunctions
         public Toggle _SelectedToggle1; //union
         public Toggle _SelectedToggle2; //difference
 
-        
 
+        
         // Start is called before the first frame update
         private void Start()
         {
             //var ItemsRight = passable.dataR.Resources.ToList();
             //var ItemsLeft = passable.dataL.Resources.ToList();
+
+
             
+
             _SelectedToggle0.onValueChanged.AddListener(delegate {
                 IntersectionValueChangedOccour(_SelectedToggle0);
             });
@@ -50,46 +53,49 @@ namespace DatAR.Widgets.MathFunctions
             });
 
         }
-
-     
-
-
-/// <summary>
-/// / Tashkhis toggle entekhab shode /////
-/// </summary>
-        
-
-        
-        
- 
-        
+            
+        public void Init()
+        {
+            //List<CooccurrenceListPassable> CoList1 = dataReceiver.dataL.ToList();
+           // List<CooccurrenceListPassable> CoList2 = dataReceiverR.dataR.ToList();
+        }
 
         void IntersectionValueChangedOccour(Toggle tgvalue)
         {
             if (tgvalue.isOn)
             {
 
-                IEnumerable<Passable> both = dataReceiver.dataL.Intersect(dataReceiverR.dataR);
+                List<Passable> CoList1 = dataReceiver.dataL as List<Passable>;
+                List<Passable> CoList2 = dataReceiverR.dataR as List<Passable>;
+                IEnumerable<Passable> intersectionResult = CoList1.AsQueryable().Intersect(CoList2);
                 // if (both.Count() < 1)
                 //{
                 // ErrorMessage = $"No Common topics found";
                 //IsLoading.OnNext(QueryState.HasError);
                 // }
                // both = new Passable<CooccurrenceListPassable>
-                dataSender.Send(both);
-                dataSenderR.Send(both);
+               // dataSender.Send(both);
+                //dataSenderR.Send(both);
             }
         }
         void UnionValueChangedOccour(Toggle tgvalue)
         {
             if (tgvalue.isOn) 
             {
-                IEnumerable<Passable> both = dataReceiver.dataL.Union(dataReceiverR.dataR);
+                List<Passable> CoList1 = dataReceiver.dataL as List<Passable>;
+                List<Passable> CoList2 = dataReceiverR.dataR as List<Passable>;
+                IEnumerable<Passable> UnionResult = CoList1.AsQueryable().Union(CoList2);
             }
         }
         void DifferenceValueChangedOccour(Toggle tgvalue)
         {
-            if (tgvalue.isOn) { }
+            if (tgvalue.isOn) 
+            {
+                List<Passable> CoList1 = dataReceiver.dataL as List<Passable>;
+                List<Passable> CoList2 = dataReceiverR.dataR as List<Passable>;
+                IEnumerable<Passable> DifferenceResult1 = CoList1.Except(CoList2);
+                IEnumerable<Passable> DifferenceResult2 = CoList2.Except(CoList1);
+            }
         }
     }
 }
